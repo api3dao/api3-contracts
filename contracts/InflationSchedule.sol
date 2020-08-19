4711a60d0fbb379e5174292c6425ffa3a90135e8
bbenligiray <bbenligiray@gmail.com>
2020-08-19 14:30:08 +0300

    Start inflation at an epoch

--- contracts/InflationSchedule.sol
+++ contracts/InflationSchedule.sol
@@ -8,13 +8,25 @@ import "./interfaces/IInflationSchedule.sol";
 contract InflationSchedule is IInflationSchedule {
     using SafeMath for uint256;
 
+    uint256 public immutable startEpoch;
+
+    constructor(uint256 _startEpoch)
+        public
+        {
+            startEpoch = _startEpoch;
+        }
+
     function getDelta(uint256 indEpoch)
         external
         view
         override
         returns(uint256 delta)
     {
-        
-        delta = uint256(100000).sub(indEpoch.mul(100)); // replace this with the actual schedule
+        if (indEpoch < startEpoch)
+        {
+            return 0;
+        }
+        uint256 passedEpochs = indEpoch - startEpoch;
+        delta = uint256(100000).sub(passedEpochs.mul(100)); // replace this with the actual schedule
     }
 }
