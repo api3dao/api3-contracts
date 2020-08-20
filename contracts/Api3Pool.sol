fe11dab88d0e34a3c92cd92a20ce4321228e9d1f
bbenligiray <bbenligiray@gmail.com>
2020-08-20 12:16:50 +0300

    Refactor

--- contracts/Api3Pool.sol
+++ contracts/Api3Pool.sol
@@ -10,12 +10,16 @@ contract Api3Pool is InterfaceUtils, EpochUtils {
     {
         address staker;
         uint256 amount;
-        uint256 unlockTimestamp;
+        uint256 vestTimestamp;
     }
   
-    mapping(address => uint256) private balances;
-    mapping(address => uint256) private lockedBalances;
-    mapping(address => uint256) private stakes;
+    mapping(address => uint256) private totalFunds;
+    // Includes initial vestings, inflationary rewards and potential insurance
+    // claim payments. Can be locked/unlocked and staked/unstaked.
+    mapping(address => uint256) private unvestedFunds;
+    // Funds locked to be staked. They are exposed to collateral risk even if
+    // they are not staked
+    mapping(address => uint256) private lockedFunds;
     mapping(address => mapping(uint256 => uint256)) private epochStakes;
     mapping(bytes32 => Vesting) private vestings;
     uint256 private noVestings;
@@ -37,15 +41,15 @@ contract Api3Pool is InterfaceUtils, EpochUtils {
         address source,
         uint256 amount,
         address beneficiary,
-        uint256 unlockTimestamp
+        uint256 vestTimestamp
         )
         external
     {
         api3Token.transferFrom(source, address(this), amount);
-        balances[beneficiary] += amount;
-        if (unlockTimestamp != 0)
+        totalFunds[beneficiary] += amount;
+        if (vestTimestamp != 0)
         {
-            lockedBalances[beneficiary] += amount;
+            unvestedFunds[beneficiary] += amount;
             bytes32 vestingId = keccak256(abi.encodePacked(
                 noVestings++,
                 this
@@ -53,7 +57,7 @@ contract Api3Pool is InterfaceUtils, EpochUtils {
             vestings[vestingId] = Vesting({
                 staker: beneficiary,
                 amount: amount,
-                unlockTimestamp: unlockTimestamp
+                vestTimestamp: vestTimestamp
             });
         }
     }
@@ -62,8 +66,8 @@ contract Api3Pool is InterfaceUtils, EpochUtils {
         external
     {
         Vesting memory vesting = vestings[vestingId];
-        require(vesting.unlockTimestamp < now, "Too early to vest");
-        lockedBalances[vesting.staker] -= vesting.amount;
+        require(vesting.vestTimestamp < now, "Too early to vest");
+        unvestedFunds[vesting.staker] -= vesting.amount;
         delete vestings[vestingId];
     }
 
@@ -74,20 +78,31 @@ contract Api3Pool is InterfaceUtils, EpochUtils {
         external
     {
         address staker = msg.sender;
-        uint256 withdrawable = balances[staker] - lockedBalances[staker] - stakes[staker];
+        uint256 unvested = unvestedFunds[staker];
+        uint256 locked = lockedFunds[staker];
+        uint256 nonWithdrawable = unvested > locked ? unvested: locked;
+        uint256 withdrawable = totalFunds[staker] -= nonWithdrawable;
         require(withdrawable >= amount, "Not enough withdrawable funds");
-        balances[staker] -= amount;
+        totalFunds[staker] -= amount;
         api3Token.transferFrom(address(this), destination, amount);
+    }
+
+    function lock(uint256 amount)
+        external
+    {
+        address staker = msg.sender;
+        uint256 lockable = totalFunds[staker] - lockedFunds[staker];
+        require(lockable >= amount, "Not enough lockable funds");
+        lockedFunds[staker] += amount;
     }
 
     function stake(uint256 amount)
         external
     {
         address staker = msg.sender;
-        uint256 stakable = balances[staker] - stakes[staker];
-        require(stakable >= amount, "Not enough stakable funds");
-        stakes[staker] += amount;
         uint256 currentEpochNumber = getCurrentEpochNumber();
-        epochStakes[staker][currentEpochNumber + 1] = stakes[staker];
+        uint256 stakeable = lockedFunds[staker] - epochStakes[staker][currentEpochNumber + 1];
+        require(stakeable >= amount, "Not enough stakeable funds");
+        epochStakes[staker][currentEpochNumber + 1] = lockedFunds[staker];
     }
 }
