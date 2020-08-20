dbf8fa3c5dd950c89c80b802f6560770ec8ec657
bbenligiray <bbenligiray@gmail.com>
2020-08-20 16:55:42 +0300

    Prettify

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,7 +1,6 @@
 const { expect } = require("chai");
 const { describe, it } = require("mocha");
 
-
 describe("Api3Token", function () {
   it("works", async function () {
     expect(true).to.equal(true);
