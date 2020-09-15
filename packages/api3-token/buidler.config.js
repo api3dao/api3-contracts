a38cd65e90f46b17fd6be9c7aa987649f4b7ab70
bbenligiray <bbenligiray@gmail.com>
2020-09-16 01:26:48 +0300

    Token deployment scripts

--- packages/api3-token/buidler.config.js
+++ packages/api3-token/buidler.config.js
@@ -1,5 +1,19 @@
+require('dotenv').config();
 usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("@nomiclabs/buidler-etherscan");
 module.exports = {
+  networks: {
+    buidlerevm: {
+    },
+    mainnet: {
+      url: process.env.MAINNET_PROVIDER_URL,
+      accounts: {mnemonic: process.env.MNEMONIC}
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_PROVIDER_URL,
+      accounts: {mnemonic: process.env.MNEMONIC}
+    },
+  },
   solc: {
     version: "0.6.12",
   },
