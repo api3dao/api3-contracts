a6ee90e0c3890d2b03bf2fddcab400c0425f20cd
rare13 <anonalpaca@protonmail.com>
2020-10-13 18:21:52 +1000

    Basic migration for timelock manager

--- packages/timelock-manager/deploy/1_TimelockManager.js
+++ packages/timelock-manager/deploy/1_TimelockManager.js
@@ -1,22 +1,91 @@
+const ethers = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
+const { deployer, utils } = require("@api3-contracts/helpers");
 module.exports = async ({ getNamedAccounts, deployments }) => {
+  ens = {
+    enabled: true,
+  };
   const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-
-  const api3DaoAddresses = {
-    1: 0,
-    4: "0x0c26bb185ad09c5a41e8fd127bf7b8c99e81e5dc",
+  // const { deployer } = await getNamedAccounts();
+  const accounts = await provider.listAccounts();
+  const signer = await provider.getSigner();
+  roles = {
+    deployer: accounts[0],
+    dao: accounts[1],
+    owner1: accounts[2],
+    owner2: accounts[3],
+    randomPerson: accounts[9],
   };
-
-  const api3TokenAddresses = {
-    1: 0,
-    4: "0x6B3998970db68A9Cb7Ab240017C20D22F08A3cC1",
-  };
-
-  const chainId = await getChainId();
-  const timelockManager = await deploy("TimelockManager", {
-    args: [api3TokenAddresses[chainId], api3DaoAddresses[chainId]],
-    from: deployer,
-  });
-
-  log(`Deployed Timelock Manager at ${timelockManager.address}`);
+  const currentTimestamp = parseInt(
+    (await provider.send("eth_getBlockByNumber", ["latest", false])).timestamp
+  );
+  timelocks = [
+    {
+      owner: roles.owner1,
+      amount: ethers.utils.parseEther((2e2).toString()),
+      releaseStart: ethers.BigNumber.from(currentTimestamp),
+      releaseEnd: ethers.BigNumber.from(currentTimestamp + 40000),
+      cliff: 0,
+      reversible: true,
+    },
+    {
+      owner: roles.owner2,
+      amount: ethers.utils.parseEther((8e2).toString()),
+      releaseStart: ethers.BigNumber.from(currentTimestamp),
+      releaseEnd: ethers.BigNumber.from(currentTimestamp + 20000),
+      cliff: 0,
+      reversible: false,
+    },
+    {
+      owner: roles.owner1,
+      amount: ethers.utils.parseEther((5e2).toString()),
+      releaseStart: ethers.BigNumber.from(currentTimestamp),
+      releaseEnd: ethers.BigNumber.from(currentTimestamp + 10000),
+      cliff: ethers.BigNumber.from(currentTimestamp + 2500),
+      reversible: false,
+    },
+    {
+      owner: roles.owner2,
+      amount: ethers.utils.parseEther((3e2).toString()),
+      releaseStart: ethers.BigNumber.from(currentTimestamp),
+      releaseEnd: ethers.BigNumber.from(currentTimestamp + 30000),
+      cliff: ethers.BigNumber.from(currentTimestamp + 10000),
+      reversible: true,
+    },
+  ];
+  api3Token = await deployer.deployToken(signer, roles.deployer);
+  log(`Deployed Token ${api3Token.address}`);
+  timelockManager = await deployer.deployTimelockManager(
+    signer,
+    roles.deployer,
+    api3Token.address
+  );
+  log(`Deployed Timelock ${timelockManager.address}`);
+  //api3Pool = await deployer.deployPool(signer, api3Token.address);
+  await api3Token.connect(signer).approve(
+    timelockManager.address,
+    timelocks.reduce(
+      (acc, timelock) => acc.add(timelock.amount),
+      ethers.BigNumber.from(0)
+    )
+  );
+  // Deploy timelocks individually
+  log(`DAO: ${roles.deployer}\n`);
+  log(`TIMELOCK OWNER: ${await timelockManager.owner()}`);
+  for (const timelock of timelocks) {
+    let tx = await timelockManager
+      .connect(signer)
+      .transferAndLock(
+        roles.deployer,
+        timelock.owner,
+        timelock.amount,
+        timelock.releaseStart,
+        timelock.releaseEnd,
+        timelock.cliff,
+        timelock.reversible
+      );
+    log(`Submitted timelock: ${timelock.owner}, ${timelock.amount}\n
+        txn: ${tx.hash}
+    `);
+  }
 };
