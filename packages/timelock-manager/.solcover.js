1148f8cf92dd166acfbf95a1808729e974e1b517
bbenligiray <bbenligiray@gmail.com>
2020-11-02 17:13:13 +0300

    Prettify

--- packages/timelock-manager/.solcover.js
+++ packages/timelock-manager/.solcover.js
@@ -1,16 +1,16 @@
-const util = require('util');
-const child = require('child_process');
+const util = require("util");
+const child = require("child_process");
 const exec = util.promisify(child.exec);
 
-async function onCompileComplete(){
-  await exec('cp -a ../api3-token/artifacts/. ./.coverage_artifacts/');
-  await exec('cp -a ../api3-pool/artifacts/. ./.coverage_artifacts/');
+async function onCompileComplete() {
+  await exec("cp -a ../api3-token/artifacts/. ./.coverage_artifacts/");
+  await exec("cp -a ../api3-pool/artifacts/. ./.coverage_artifacts/");
 }
 
 module.exports = {
   onCompileComplete,
   mocha: {
-    grep: '(test uses evm_setNextBlockTimestamp)',
-    invert: true
-  }
-}
+    grep: "(test uses evm_setNextBlockTimestamp)",
+    invert: true,
+  },
+};
